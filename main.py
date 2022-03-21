3b7b39fc9b6d6d355689ec25eb730853aa69a241
Y4NG333 <91429283+Y4NG333@users.noreply.github.com>
2022-03-21 22:58:24 +0900

    Modified the main.py
    1.Use one-hot encoding 
    2.Add drawing function

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import torch.utils.data as Data
 
 from matplotlib import pyplot as plt
 from model import Transformer
-from utils import data_generator, DataInfo
+from utils import data_generator, DataInfo, draw_plot
 
 # Used to configure the environment
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
@@ -17,7 +17,7 @@ alphabet = ["a", "b", "c"]
 weight = np.array([2, 1, 1])
 prob = weight / np.sum(weight)
 
-num_epoch = 5
+num_epoch = 15
 batch_num = 50
 seq_len = 5
 max_len = 12
@@ -38,19 +38,21 @@ datainfo = DataInfo(
 # Define model
 n_heads, d_model, n_layers, src_vocab_size, tgt_vocab_size = 8, 512, 6, len(alphabet), 3
 model = Transformer(n_heads, d_model, n_layers, src_vocab_size, tgt_vocab_size)
-criterion = nn.CrossEntropyLoss(ignore_index=0)
+criterion = nn.MSELoss()
 optimizer = optim.SGD(model.parameters(), lr=1e-3, momentum=0.99)
 
 # Train
 for epoch in range(num_epoch):
-    seq, seq_int, code, code_int = data_generator(datainfo, batch_num)
+    seq, seq_int, code, code_int, code_onehot = data_generator(datainfo, batch_num)
     seq_int = torch.LongTensor(seq_int)
     code_int = torch.LongTensor(code_int)
 
     optimizer.zero_grad()
     outputs, enc_self_attns, dec_self_attns, dec_enc_attns = model(seq_int, code_int)
-    loss = criterion(outputs, code_int.view(-1))
+    loss = criterion(outputs.float(), torch.LongTensor(code_onehot).view(-1, 3).float())
     loss.backward()
     optimizer.step()
 
+    if epoch == 10:
+        draw_plot(outputs, code_int.view(-1), dec_enc_attns, seq)
     print("Epoch:", "%04d" % (epoch + 1), "loss =", f"{loss}")
