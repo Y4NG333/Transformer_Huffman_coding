f0850fe8b7593736b27013be755d27229e0d0f72
Y4NG333 <91429283+Y4NG333@users.noreply.github.com>
2022-03-26 19:57:23 +0900

    using dimension variables
    3 --> dimension

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ seq_len = 5
 max_len = 12
 pad_symbol = "2"
 test_nums = 10
+dimension = 3
 fname = "./model/20net.pth"
 
 weighted_tuple = [(alphabet[i], weight[i]) for i in range(len(alphabet))]
@@ -53,5 +54,5 @@ real_out = [torch.argmax(x).item() for x in outputs]
 
 print("real out", np.array(real_out).reshape(-1, max_len))
 print("code int", code_int)
-loss = criterion(outputs.float(), torch.LongTensor(code_onehot).view(-1, 3).float())
+loss = criterion(outputs.float(), torch.LongTensor(code_onehot).view(-1, dimension).float())
 print("loss =", f"{loss}")
