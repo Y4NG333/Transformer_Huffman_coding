4952d173077d7431208a1a4e4c7093d93d0ab37d
Y4NG333 <91429283+Y4NG333@users.noreply.github.com>
2022-04-18 23:12:27 +0900

    add inference function
    Complete output tasks without enc_input

--- test.py
+++ test.py
@@ -18,6 +18,7 @@ parser.add_argument("--sentence", type=str, default="abcab", help="length 5 sequ
 parser.add_argument("--fname", type=str, default="./model/300net.pth", help="the name of the model ")
 opt = parser.parse_args()
 
+# generate the test dataset
 alphabet = ["a", "b", "c"]
 weight = np.array([2, 1, 1])
 prob = weight / np.sum(weight)
@@ -61,3 +62,37 @@ real_out = [torch.argmax(x).item() for x in outputs]
 loss = criterion(outputs, torch.LongTensor(code_int_c).view(-1))  # code_int.view(-1)
 print("loss =", f"{loss}")
 draw_plot(outputs, torch.LongTensor(code_int_c).view(-1), dec_enc_attns, seq)
+
+
+def inference(sentence):
+    sentence_n = [item for item in sentence]
+    code_symbolwise = np.vectorize(datainfo.codebook.__getitem__)(sentence_n)
+    seq_chr_map = {c: i for i, c in enumerate(datainfo.alphabet)}
+    seq_int = np.vectorize(seq_chr_map.get)(sentence_n)
+    seq_int += 1
+    print("input", seq_int)
+
+    # encoder
+    seq_int = torch.LongTensor(seq_int).view(1, 5)
+    with torch.no_grad():
+        enc_outputs, enc_self_attns = model_test.encoder(seq_int)
+
+    # get dec_input
+    dec_input = torch.zeros(1, 0).type_as(seq_int.data)
+    next_symbol = 3
+    for i in range(max_len):
+        dec_input = torch.cat([dec_input.detach(), torch.tensor([[next_symbol]])], -1)
+        dec_outputs, dec_self_attns, dec_enc_attns = model_test.decoder(dec_input, seq_int, enc_outputs)
+        projected = model_test.projection(dec_outputs)
+        prob = projected.squeeze(0).max(dim=-1, keepdim=False)[1]
+        next_word = prob.data[-1]
+        next_symbol = next_word
+
+    # output
+    predict, _, _, _ = model_test(seq_int, dec_input)
+    predict = predict.data.max(1, keepdim=True)[1]
+    print("output", [n.item() for n in predict.squeeze()])
+
+
+print("translate_sentence")
+inference(opt.sentence)
